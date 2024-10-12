d4f9e668b60f6fe541a8a187040f688938f42eac
Himanshu Dwivedi <hdwivedi96@gmail.com>
2024-10-12 18:54:19 +0530

    polymorphism achieved

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
 
+        Product p=new Product();
+        int iRes2=p.product(10,3);
+       int iRes3= p.product(10,3,7);
+       double Res= p.product(10.4,8.4);
+       System.out.print(iRes2+" "+iRes3+" "+Res);
+    }
 }

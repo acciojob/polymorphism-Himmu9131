007a6da01ce4278038b7953186c4d0ea0edc4e57
Himanshu Dwivedi <hdwivedi96@gmail.com>
2024-10-13 12:38:11 +0530

    updated poly

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,10 +3,23 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
 
-        Product p=new Product();
-        int iRes2=p.product(10,3);
-       int iRes3= p.product(10,3,7);
-       double Res= p.product(10.4,8.4);
+        Product p = new Product();
+        p.product(10, 3);
+        p.product(10, 3, 7);
+        p.product(10.4, 8.4);
 
     }
 }
+    class Product{
+        public int product(int x, int y) {
+            return x*y;
+        }
+
+        public double product(double x, double y) {
+            return x*y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+    }
